b49710f058c721ed6dba7317496ee3ac22e130cc
Junno Beringuela <junno.paolo.beringuela@oocl.com>
2017-08-04 13:45:25 +0800

    test branch commmit

--- GitDemo/src/main/java/com/oocl/group3/GitDemo/App.java
+++ GitDemo/src/main/java/com/oocl/group3/GitDemo/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //System.out.println("This is from test branch.");
+        System.out.println("This is from another branch.");
     }
 }
