d2fd13d10217ec29736e31a71ec3d0d4e7ba54c6
Junno Beringuela <junno.paolo.beringuela@oocl.com>
2017-08-04 11:26:44 +0800

    Initial Commit

---
+++ GitDemo/src/main/java/com/oocl/group3/GitDemo/App.java
@@ -0,0 +1,13 @@
+package com.oocl.group3.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
