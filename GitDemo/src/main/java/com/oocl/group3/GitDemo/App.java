7f1ccbb8b07f975c6abc754ce6650f54b90a814d
Junno Beringuela <junno.paolo.beringuela@oocl.com>
2017-08-04 14:02:04 +0800

    fgh

--- GitDemo/src/main/java/com/oocl/group3/GitDemo/App.java
+++ GitDemo/src/main/java/com/oocl/group3/GitDemo/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("This is from test branch.");
-        //System.out.println("This is from another branch.");
+        System.out.println("hi");
     }
 }
